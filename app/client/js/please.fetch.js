207a04d42ff1382c1172fab513942a362b1b258e
Eric Mignot <eric.mignot@gmail.com>
2018-03-09 16:54:42 -0800

    Trying http json api serving message from database

--- app/client/js/please.fetch.js
+++
@@ -1,9 +0,0 @@
-var pleaseFetch = function(url, done) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function() {
-        var data = xhr.responseText;
-        done(data);
-    };
-    xhr.send();
-};
--- start.js
+++ start.js
@@ -1,13 +1,8 @@
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var home = 'http://' + ip + ':' + port;
-
-var FakeBceIDServer = require('./app/server/fake.bceid.server');
-var fakeBceIDServer = new FakeBceIDServer({token:'monday'});
-
 var Server = require('./app/server/server');
 var server = new Server();
-server.useBceidServer(fakeBceIDServer);
 
 server.start(port, ip, function() {
     console.log(ip + ' listening on port ' + port);
@@ -17,12 +12,15 @@ var pg = require('pg');
 var client = new pg.Client();
 
 client.connect(function(err) {
-    console.log('connection status: ' + JSON.stringify(err));
-    var sql = 'select content from messages';
-    client.query(sql, function(err, result) {
-        client.end();
-        console.log('RESULT: ' + JSON.stringify(result));
-    });
+    console.log('postgresql connection status: ' + JSON.stringify(err));
+    if (!err) {
+        var sql = 'select content from messages';
+        client.query(sql, function(err, result) {
+            client.end();
+            console.log('RESULT: ' + JSON.stringify(result));
+            server.setMessage(result.rows[0].content);
+        });
+    }
 });
 
 module.exports = server;
