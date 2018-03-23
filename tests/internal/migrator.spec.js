0d9105d9252e3c44fad857bf84c84149d0770aaf
Eric Mignot <eric.mignot@gmail.com>
2018-03-23 09:44:54 -0700

    Increased quality of Migrator spec

--- tests/internal/migrator.spec.js
+++ tests/internal/migrator.spec.js
@@ -2,39 +2,61 @@ var expect = require('chai').expect;
 var Database = require('../../app/database');
 var pg = require('pg');
 var Migrator = require('../../app/migrations/migrator');
-var Truncator = require('../support/truncator');
 
-describe('Save form', function() {
+describe('Migrator', function() {
 
     var database;
     var connectedToLocalhost = function() {
         return new pg.Client('postgres://postgres@localhost/e-filing');
     };
+    var migrator;
 
-    beforeEach(function(done) {
+    beforeEach(function() {
         database = new Database(connectedToLocalhost);
-        var migrator = new Migrator(connectedToLocalhost);
-        migrator.migrateNow(function() {
-            var truncator = new Truncator(connectedToLocalhost);
-            truncator.truncateTablesNow(function() {
-                done();
+        migrator = new Migrator(connectedToLocalhost);        
+    });
+
+    it('creates the record when missing', function(done) {
+        var client = connectedToLocalhost();    
+        client.connect(function(err) {
+            if (err) { throw err; }
+            client.query('truncate table versions', function(err, result) {
+                if (err) { throw err; }
+                migrator.migrateNow(function() {
+                    client.query('SELECT id FROM versions', function(err, result) {
+                        expect(err).to.equal(null);
+                        expect(result.rows.length).to.equal(1);
+                        var { id } = result.rows[0];
+                        
+                        expect(id).to.equal(2);
+                        client.end();
+                        done();
+                    });
+                });
             });
         });
     });
 
-    it('updates database version record', function(done) {
-        var client = connectedToLocalhost();
-        client.connect(function(err) {                
-            expect(err).to.equal(null);
-            var sql = 'SELECT id FROM versions';
-            client.query(sql, function(err, result) {
-                expect(err).to.equal(null);
-                expect(result.rows.length).to.equal(1);
-                var { id } = result.rows[0];
-                
-                expect(id).to.equal(2);
-                client.end();
-                done();
+    it('updates the record when it exists', function(done) {
+        var client = connectedToLocalhost();    
+        client.connect(function(err) {
+            if (err) { throw err; }
+            client.query('truncate table versions', function(err, result) {
+                if (err) { throw err; }
+                client.query('insert into versions(id) values(1);', function(err, result) {
+                    expect(err).to.equal(null);
+                    migrator.migrateNow(function() {                    
+                        client.query('SELECT id FROM versions', function(err, result) {
+                            expect(err).to.equal(null);
+                            expect(result.rows.length).to.equal(1);
+                            var { id } = result.rows[0];
+                            
+                            expect(id).to.equal(2);
+                            client.end();
+                            done();
+                        });
+                    });
+                });                
             });
         });
     });
