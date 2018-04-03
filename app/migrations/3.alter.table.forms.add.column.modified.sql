31f93a94e7216c42249fcec33a5c652a8df3e889
Eric Mignot <eric.mignot@gmail.com>
2018-04-03 16:34:05 -0700

    postgres v9 does not have the add column if exists feature

--- app/migrations/3.alter.table.forms.add.column.modified.sql
+++ app/migrations/3.alter.table.forms.add.column.modified.sql
@@ -1,1 +1,14 @@
-alter table forms add column if not exists modified timestamp default current_timestamp;
+DO
+$do$
+BEGIN
+    IF NOT EXISTS (
+        select column_name 
+        FROM information_schema.columns 
+        where table_name='forms' and column_name='modified'
+    ) THEN
+        alter table forms add column modified timestamp default current_timestamp;
+    END IF;
+END
+$do$
+;
+
