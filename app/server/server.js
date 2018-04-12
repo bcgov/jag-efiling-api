e6790212568e46f39a2442d88b00b2f310050cab
Eric Mignot <eric.mignot@gmail.com>
2018-04-12 10:49:20 -0700

    cors middleware

--- app/server/server.js
+++ app/server/server.js
@@ -8,6 +8,10 @@ function Server() {
 };
 
 Server.prototype.start = function (port, ip, done) {
+    this.app.use((request, response, next)=>{
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Content-Type', 'application/json');
+    });
     this.app.use(bodyParser.urlencoded({ extended: false }));    
     this.restAdaptor.route(this.app);
     this.server = this.app.listen(port, ip, done);
