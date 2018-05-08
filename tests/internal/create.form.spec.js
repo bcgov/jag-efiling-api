71769d9531228799e722bcc8eb6d27d6e1c9809c
Eric Mignot <eric.mignot@gmail.com>
2018-05-08 14:02:03 -0700

    internal tests back to green

--- tests/internal/create.form.spec.js
+++ tests/internal/create.form.spec.js
@@ -3,14 +3,17 @@ var Database = require('../../app/store/database');
 var Migrator = require('../../app/migrations/migrator');
 var Truncator = require('../support/truncator');
 var { Forms } = require('../../app/store/forms');
+var CreateFormTwo = require('../../app/features/create.form.2');
 
 describe('Create form', function() {
 
     var database;
     var forms;
+    var createFormTwo;
 
-    beforeEach(function(success) {
+    beforeEach(function(success) {        
         database = new Database();
+        createFormTwo = new CreateFormTwo(database);
         forms = new Forms();
         var migrator = new Migrator();
         migrator.migrateNow(function() {
@@ -26,12 +29,11 @@ describe('Create form', function() {
             type: 'form-2',
             data: { value:42 }
         };
-        database.createForm(form, function(newId) {
+        createFormTwo.now(form, function(newId) {
             expect(newId).not.to.equal(undefined);
-            forms.selectAll(function(rows) {
+            forms.selectOne(newId, function(rows) {
                 expect(rows.length).to.equal(1);
-                var { id, type, status, data } = rows[0];
-                expect(id).to.equal(newId);
+                var { type, status, data } = rows[0];
                 expect(type).to.equal('form-2');
                 expect(status).to.equal('Draft');
                 expect(data).to.equal(JSON.stringify({ value:42 }));
