c117a8a11e7ceb83abb464317bf1129a6112ccb3
Eric Mignot <eric.mignot@gmail.com>
2018-05-09 10:08:44 -0700

    Extract renderers to clean up rest.adaptor

---
+++ app/features/index.js
@@ -0,0 +1,15 @@
+let SearchFormSeven = require('./search.form.7');
+let MyCases = require('./my.cases');
+let CreateFormTwo = require('./create.form.2');
+let SavePerson = require('./save.person');
+let PersonInfo = require('./person.info');
+let UpdateFormTwo = require('./update.form.2');
+
+module.exports = {
+    SearchFormSeven:SearchFormSeven,
+    MyCases:MyCases,
+    CreateFormTwo:CreateFormTwo,
+    SavePerson:SavePerson,
+    PersonInfo:PersonInfo,
+    UpdateFormTwo:UpdateFormTwo
+}
