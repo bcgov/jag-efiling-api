18df9572137068442f4f5c646a07ea88742e1819
perkinss <perkins.susan@gmail.com>
2018-05-31 14:26:09 -0700

    try to fix travis build for real

--- tests/external/hub.unavailable.spec.js
+++ tests/external/hub.unavailable.spec.js
@@ -8,12 +8,8 @@ describe('Hub unavailable', function() {
     var port = 8111;
     var broken = 'http://bob' + ":" + port;
 
-    beforeEach((done)=> {
+    beforeEach(()=> {
         brokenhub = new Hub(broken);
-    });
-
-    afterEach(function(done) {
-        server.close(done);
     });
 
     it('handles internet outages gracefully', function(done) {
