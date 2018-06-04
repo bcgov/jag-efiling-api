8050dd2bd51e1259ac95e59f911f943e8acce6b9
perkinss <perkins.susan@gmail.com>
2018-06-04 09:48:30 -0700

    * Update package.json so that internal tests do not use default timeout * Update tests to check response to a 5 second socket timeout * Update code to timeout if a response is not recieved in 2 seconds

--- app/hub/hub.js
+++ app/hub/hub.js
@@ -7,28 +7,25 @@ function Hub(url) {
 Hub.prototype.searchForm7 = function(file, callback) {    
     var target = this.url + '/form7s?caseNumber='+file;
 
-    request(target, function(err, response, body) {        
+    request(target, {timeout: 2000}, function(err, response, body) {
+
         if (err) {
-            callback('503:SERVICE UNAVAILABLE');            
-        } 
-        else {
-            if (response.statusCode === 200) {
-                var data = JSON.parse(body);
-                var parties = extractParties(data);
-                if (parties) {
-                    callback({
-                        appellants: rawAppellants(parties).map(buildPartyInfo),
-                        respondents: rawRespondents(parties).map(buildPartyInfo)
-                    });
-                }
-                else {
-                    callback('404:NOT FOUND');
-                }
+            callback("503:SERVICE UNAVAILABLE");
+        } else if (response.statusCode === 404) {
+            callback('404:NOT FOUND');
+        } else if (response.statusCode === 200) {
+            var data = JSON.parse(body);
+            var parties = extractParties(data);
+            if (parties) {
+                callback({
+                    appellants: rawAppellants(parties).map(buildPartyInfo),
+                    respondents: rawRespondents(parties).map(buildPartyInfo)
+                });
             }
             else {
                 callback('404:NOT FOUND');
-            }    
-        }        
+            }
+        }
     });    
 };
 
