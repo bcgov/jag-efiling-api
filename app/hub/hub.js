d735bd7698b850bcb81b319b26754c147f38f740
Eric Mignot <eric.mignot@gmail.com>
2018-06-05 10:07:49 -0700

    Back to 100% green

--- app/hub/hub.js
+++ app/hub/hub.js
@@ -10,17 +10,21 @@ Hub.prototype.searchForm7 = function(file, callback) {
     request(target, {timeout: 2000 }, function(err, response, body) {
 
         if (err) {
-            callback("503:SERVICE UNAVAILABLE");
-        } else if (response.statusCode === 404) {
-            callback('404:NOT FOUND');
-        } else if (response.statusCode === 200) {
-            var data = JSON.parse(body);
-            var parties = extractParties(data);
-            if (parties) {
-                callback({
-                    appellants: rawAppellants(parties).map(buildPartyInfo),
-                    respondents: rawRespondents(parties).map(buildPartyInfo)
-                });
+            callback('503:SERVICE UNAVAILABLE');            
+        } 
+        else {
+            if (response.statusCode === 200) {
+                var data = JSON.parse(body);
+                var parties = extractParties(data);
+                if (parties) {
+                    callback({
+                        appellants: rawAppellants(parties).map(buildPartyInfo),
+                        respondents: rawRespondents(parties).map(buildPartyInfo)
+                    });
+                }
+                else {
+                    callback('404:NOT FOUND');
+                }
             }
             else {
                 callback('404:NOT FOUND');
