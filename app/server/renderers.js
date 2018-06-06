86581ccb29e8e745c8b917690d4984ffbcbb6e70
Eric Mignot <eric.mignot@gmail.com>
2018-06-06 14:56:12 -0700

    extract render function to remove Duplication in renderers

--- app/server/renderers.js
+++ app/server/renderers.js
@@ -1,11 +1,20 @@
 let render503 = function(response) {
     response.statusCode = 503;   
-    response.write(JSON.stringify({message:'service unavailable'}));     
+    response.write(JSON.stringify({message:'service unavailable'})); 
+    response.end();     
 };
 let render404 = function(response) {
     response.statusCode = 404;
-    response.write(JSON.stringify({message:'not found'}));     
+    response.end(JSON.stringify({message:'not found'}));   
 };
+let render = function(data, response, orContinue) {
+    if (data.error) {
+        render503(response);
+    }
+    else {
+        orContinue(data, response);
+    }
+}
 let renderSearchFormSevenResult = function(data, response) { 
     if (data == '404:NOT FOUND') {
         render404(response);
@@ -13,67 +22,47 @@ let renderSearchFormSevenResult = function(data, response) {
     else if (data == '503:SERVICE UNAVAILABLE') {
         render503(response);
     } else {
-        response.write( JSON.stringify({ parties:data })); 
-    }        
-    response.end(); 
+        response.end( JSON.stringify({ parties:data })); 
+    }            
 };
 let renderMyCasesResult = function(cases, response) { 
-    if (cases.error) {
-        render503(response);
-    }
-    else {
-        response.write( JSON.stringify({ cases:cases })); 
-    }    
-    response.end(); 
+    render(cases, response, (cases, response) => { 
+        response.end( JSON.stringify({ cases:cases })); 
+    });      
 };    
 let renderCreateFormTwoResult = function(id, response) {
-    if (id.error) {
-        render503(response);
-    }
-    else {
+    render(id, response, (cases, response) => { 
         response.writeHead(201, {'Location': '/forms/' + id});
-        response.write(JSON.stringify({}));
-    }
-    response.end();
+        response.end(JSON.stringify({}));
+    });
 };
 let renderUpdateFormTwoResult = function(id, response) {
-    if (id.error) {
-        render503(response);
-    }
-    else {
+    render(id, response, (cases, response) => { 
         response.writeHead(200, {'Location': '/forms/' + id});
-    }
-    response.end();
+        response.end();
+    });  
 };
 let renderSavePersonResult = function(id, response) { 
-    if (id.error) {
-        render503(response);
-    }
-    else {
+    render(id, response, (cases, response) => { 
         response.writeHead(201, {'Location': '/persons/' + id});
-        response.write(JSON.stringify({}));
-    }
-    response.end();
+        response.end(JSON.stringify({}));
+    });
 };
 let renderPersonInfoResult = function(person, response) { 
     if (person !== undefined) {
-        if (person.error) {
-            render503(response);
-        }
-        else {
-            response.write(JSON.stringify(person));
-        }
+        render(person, response, (cases, response) => { 
+            response.end(JSON.stringify(person));
+        });
     } 
     else {
         response.statusCode = 404;
-    }
-    response.end();
+        response.end();
+    }    
 };
 let renderArchiveCasesResult = function(data, response) {
-    if (data.error) {
-        render503(response);
-    }
-    response.end();
+    render(data, response, (cases, response) => { 
+        response.end();
+    });
 };
 
 module.exports = {
