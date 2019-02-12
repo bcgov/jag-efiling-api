5875db82bad481ac6776d521a1ce01f2ed7dfa20
perkinss <perkins.susan@gmail.com>
2019-02-11 16:52:04 -0800

    Add support for multiple respondents in form 2 preview

--- app/features/preview.form2.js
+++ app/features/preview.form2.js
@@ -9,20 +9,21 @@ PreviewForm2.prototype.now = function(id, callback) {
     this.database.formData(id, (data)=>{
         if (data.error) { callback(data); }
         else {
+            const selected = this.filterSelected(data.respondents);
             var html = this.template
                 .replace('{formSevenNumber}', data.formSevenNumber)
                 .replace('{appellants}', this.reduce(data.appellants))
                 .replace('{appellantsLabel}', 'Appellant' + this.label(data.appellants))
-                .replace('{respondents}', this.reduce(data.respondents))
-                .replace('{respondentsLabel}', 'Respondent' + this.label(data.respondents))
-                .replace('{selectedRespondentName}', data.respondents[data.selectedRespondentIndex].name)
-                .replace('{selectedRespondentAddress1}', this.extract('addressLine1', data))
-                .replace('{selectedRespondentAddress2}', this.extract('addressLine2', data))
-                .replace('{selectedRespondentCity}', this.extract('city', data))
-                .replace('{selectedRespondentProvince}', this.extract('province', data))
-                .replace('{selectedRespondentPostalCode}', this.extract('postalCode', data))
-                .replace('{selectedRespondentPhone}', data.phone)
-                .replace('{selectedRespondentEmail}', data.useServiceEmail ? data.email : '')
+                .replace('{respondents}', this.reduce(selected))
+                .replace('{respondentsLabel}', 'Respondent' + this.label(selected))
+                .replace('{selectedContactName}', data.respondents[data.selectedContactIndex].name)
+                .replace('{selectedContactAddress1}', this.extract('addressLine1', data))
+                .replace('{selectedContactAddress2}', this.extract('addressLine2', data))
+                .replace('{selectedContactCity}', this.extract('city', data))
+                .replace('{selectedContactProvince}', this.extract('province', data))
+                .replace('{selectedContactPostalCode}', this.extract('postalCode', data))
+                .replace('{selectedContactPhone}', data.phone)
+                .replace('{selectedContactEmail}', data.useServiceEmail ? data.email : '')
                 ;
             if (!data.useServiceEmail) {
                 html = this.removeIfBlock('useServiceEmail', html);
@@ -37,11 +38,15 @@ PreviewForm2.prototype.now = function(id, callback) {
 PreviewForm2.prototype.reduce = function(array) {
     return array.reduce((result, item, index)=> result + (index > 0 ? ', ': '') + item.name, '');
 };
+
+PreviewForm2.prototype.filterSelected = function(array) {
+    return array.filter((item)=> item.selected);
+};
 PreviewForm2.prototype.label = function(array) {
     return array.length > 1 ? 's': '';
 };
 PreviewForm2.prototype.address = function(data) {
-    return data.respondents[data.selectedRespondentIndex].address;
+    return data.respondents[data.selectedContactIndex].address;
 };
 PreviewForm2.prototype.extract = function(field, data) {
     return  this.address(data) && this.address(data)[field] ? this.address(data)[field] : '';
