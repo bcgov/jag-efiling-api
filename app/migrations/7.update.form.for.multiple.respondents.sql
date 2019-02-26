cf64c7a18f4a687f1b4ff8e795074511075f0993
Eric Mignot <eric.mignot@gmail.com>
2019-02-25 17:00:01 -0800

    migrator now runs migration #7

--- app/migrations/7.update.form.for.multiple.respondents.sql
+++ app/migrations/7.update.form.for.multiple.respondents.sql
@@ -2,7 +2,8 @@ DO
 $do$
 BEGIN
      UPDATE forms
-     SET data = REPLACE(data, 'selectedRespondent', 'selectedContact');
+     SET data = REPLACE(data, 'selectedRespondent', 'selectedContact')
+     WHERE 1=1;
 END
 $do$
 ;
