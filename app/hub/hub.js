f5556a9e3da9687c062bdbf2e5560b61c5e9f2da
Eric Mignot <eric.mignot@gmail.com>
2019-03-26 11:16:56 -0700

    app now uses http.request instead of request package

--- app/hub/hub.js
+++ app/hub/hub.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var { request } = require('http');
 var {
     extractParties,
     buildPartyInfo,
@@ -13,37 +13,52 @@ function Hub(url, timeout) {
 }
 Hub.prototype.searchForm7 = function(file, callback) {
     var target = this.url + '/form7s?caseNumber='+file;
+    var timedout = false
+    var search = request(target, {timeout: this.timeout }, function(response) {
+        if (timedout) { return }
 
-    request(target, {timeout: this.timeout }, function(err, response, body) {
-
-        if (err || response.statusCode == 503) {
+        if (response.statusCode === 503) {
             callback({ error: {code:503} });
         }
         else {
             if (response.statusCode === 200) {
-                var data = JSON.parse(body);
-                var caseType = extractCaseType(data)
-                if (caseType == 'Criminal') {
-                    callback({ error: {code:404, message:'criminal'} });
-                }
-                else {
-                    var parties = extractParties(data);
-                    if (parties) {
-                        callback({
-                            appellants: rawAppellants(parties).map(buildPartyInfo),
-                            respondents: rawRespondents(parties).map(buildPartyInfo)
-                        });
+                var body = ''
+                response.on('data', (chunk) => {
+                    body += chunk
+                })
+                response.on('end', () => {
+                    var data = JSON.parse(body);
+                    var caseType = extractCaseType(data)
+                    if (caseType == 'Criminal') {
+                        callback({ error: {code:404, message:'criminal'} });
                     }
                     else {
-                        callback({ error: {code:404} });
+                        var parties = extractParties(data);
+                        if (parties) {
+                            callback({
+                                appellants: rawAppellants(parties).map(buildPartyInfo),
+                                respondents: rawRespondents(parties).map(buildPartyInfo)
+                            });
+                        }
+                        else {
+                            callback({ error: {code:404} });
+                        }
                     }
-                }
+                })
             }
             else {
                 callback({ error: {code:404} });
             }
         }
-    });
+    })
+    search.on('error', (err)=>{
+        callback({ error: {code:503} })
+    })
+    search.on('timeout', (err)=>{
+        timedout = true
+        callback({ error: {code:503} })
+    })
+    search.end()
 };
 
 module.exports = Hub;
