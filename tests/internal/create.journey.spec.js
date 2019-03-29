04b2e2ed2f089e79af5d8b52ee65b5f48712cff3
perkinss <perkins.susan@gmail.com>
2019-03-29 11:39:37 -0700

    Ensure only one journey is created per user

--- tests/internal/create.journey.spec.js
+++ tests/internal/create.journey.spec.js
@@ -51,4 +51,46 @@ describe('Create journey', function() {
             });
         });
     });
+
+    it('creates only one journey per user', function(done) {
+
+        savePerson.now('jane', function(newUserId) {
+            var testJourney = {
+                type: 'respondtoleavetoappeal',
+                state: 'started',
+                ca_number: 'CA1234',
+                userid: newUserId,
+                steps: "unimportant"
+            };
+            var modifiedJourney = {
+                type: 'appellantrighttoappeal',
+                state: 'completed',
+                ca_number: 'CA1234',
+                userid: newUserId,
+                steps: "blah blah blah"
+            }
+            createJourney.now(testJourney, function(journey_id) {
+                console.log("Created first journey", journey_id)
+                expect(journey_id).not.to.equal(undefined);
+                journey.selectAll(function(rows) {
+                    expect(rows.length).to.equal(1);
+                    expect(rows[0].id).to.equal(Number(journey_id));
+                    createJourney.now(modifiedJourney, function(next_journey_id) {
+                        expect(next_journey_id).not.to.equal(undefined);
+                        expect(next_journey_id).to.equal(journey_id);
+                        journey.selectAll(function(rows) {
+                            expect(rows.length).to.equal(1);
+                            expect(rows[0].id).to.equal(Number(journey_id));
+                            expect(rows[0].type).to.equal('appellantrighttoappeal');
+                            expect(rows[0].userid).to.equal(Number(newUserId));
+                            expect(rows[0].state).to.equal('completed');
+                            expect(rows[0].steps).to.equal('blah blah blah');
+                            expect(rows[0].ca_number).to.equal('CA1234');
+                            done()
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
