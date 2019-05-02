fa2c3076cf886527d1a47cd10cb139b8acfce68d
Eric Mignot <eric.mignot@gmail.com>
2019-05-02 10:33:08 -0700

    folders for pages and components

--- app/server/client.adaptor.js
+++ app/server/client.adaptor.js
@@ -3,6 +3,19 @@ let path = require('path')
 let express = require('express')
 let ClientAdaptor = function() {};
 
+var folder = function(...name) {
+    let content = ''
+    let fullName = path.join.apply(null, name)
+    let files = fs.readdirSync(fullName)
+    for (var i=0; i<files.length; i++) {
+        if (files[i].endsWith('.js')) {
+            var file = path.join.apply(null, name.concat(files[i]))
+            content += fs.readFileSync(file)
+        }
+    }
+    return content
+}
+
 ClientAdaptor.prototype.route = function(app) {
     app.use('/client/css', express.static(path.join(__dirname, '..', 'client', 'css')));
     app.use('/client/images', express.static(path.join(__dirname, '..', 'client', 'images')));
@@ -10,15 +23,10 @@ ClientAdaptor.prototype.route = function(app) {
     app.get('/client/all.js', (request, response)=> {
         response.writeHead(200, { 'content-type':'application/javascript' })
         let js = ''
-        let files = fs.readdirSync(path.join(__dirname, '..', 'client', 'js', 'yop'))
-        for (var i=0; i<files.length; i++) {
-            js += fs.readFileSync(path.join(__dirname, '..', 'client', 'js', 'yop', files[i]))
-        }
-        files = fs.readdirSync(path.join(__dirname, '..', 'client', 'js'))
-                    .filter((file)=> file.endsWith('.js'))
-        for (var i=0; i<files.length; i++) {
-            js += fs.readFileSync(path.join(__dirname, '..', 'client', 'js', files[i]))
-        }
+        js += folder(__dirname, '..', 'client', 'js', 'yop')
+        js += folder(__dirname, '..', 'client', 'js')
+        js += folder(__dirname, '..', 'client', 'js', 'pages', 'components')
+        js += folder(__dirname, '..', 'client', 'js', 'pages')
         response.end(js)
     });
     app.get('/client*', (request, response)=> {
