9f1e4dbbb742e81003194f07acafdc17db10908f
Eric Mignot <eric.mignot@gmail.com>
2019-05-21 09:24:43 -0700

    fake credentials can now be passed in command line

--- start.with.fake.credentials.js
+++ start.with.fake.credentials.js
@@ -5,13 +5,17 @@ setTimeout(function() {
     server.stop(()=>{
         let ip = server.ip;
         let port = server.port;
-        server.requestheaders.push({ name:'smgov_userguid', value:'JD' });
-        server.requestheaders.push({ name:'smgov_userdisplayname', value:'Doe, John' });
+        console.log('argv', process.argv);
+        let guid = process.argv[2] || 'JD'
+        let name = process.argv[3] || 'Doe, John'
+        console.log('using', 'guid:', guid, 'name:', name);
+        server.requestheaders.push({ name:'smgov_userguid', value:guid });
+        server.requestheaders.push({ name:'smgov_userdisplayname', value:name });
         server.start(port, ip, function() {
             console.log(ip + ' listening on port ' + port + ' with fake credentials');
         });
     });
-}, 300);
+}, 900);
 
 module.exports = server;
 module.exports.port = server.port;
